1cf14c2a96816ad8822b85c847913465856479e1
smile1225 <1679203148@qq.com>
2016-04-29 08:35:53 +0800

    提交

---
+++ js/isotop_ini.js
@@ -0,0 +1,70 @@
+$( function() {
+  // init Isotope
+  var $container = $('#isotope').isotope({
+    itemSelector: '.element',
+    layoutMode: 'fitRows',
+    getSortData: {
+      name: '.name',
+      symbol: '.symbol',
+      number: '.number parseInt',
+      category: '[data-category]',
+      weight: function( itemElem ) {
+        var weight = $( itemElem ).find('.weight').text();
+        return parseFloat( weight.replace( /[\(\)]/g, '') );
+      }
+    }
+  });
+
+  // filter functions
+  var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function() {
+      var number = $(this).find('.number').text();
+      return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function() {
+      var name = $(this).find('.name').text();
+      return name.match( /ium$/ );
+    }
+  };
+
+  // bind filter button click
+  $('#filters').on( 'click', '.btn', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $container.isotope({ filter: filterValue });
+  });
+  
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', '.btn', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+
+      return false;
+    });
+  });
+  
+});
+$("#more1").click(function(){
+  window.location.href="aosika/index.html"
+})
+$("#more2").click(function(){
+  window.location.href="http://aotulink.com/"
+})
+$("#more3").click(function(){
+  window.location.href="http://www.miliao.com/"
+})
+$("#more4").click(function(){
+  window.location.href="https://jr.jiayuan.com/"
+})
+$("#more5").click(function(){
+  window.location.href="http://www.epaidai.com/"
+})
+$("#more6").click(function(){
+  window.location.href="caomao/index.html"
+})
+
